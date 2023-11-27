1b09e15be75baee54ebd86ded33d94ee40067138
YifeiDeng123 <114625926+YifeiDeng123@users.noreply.github.com>
2023-11-27 12:30:11 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -19,3 +19,57 @@ public class TestListExamples {
     assertEquals(expected, merged);
   }
 }
+
+@Test(timeout = 500)
+  public void testMerge2() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("b", "d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMerge3() {
+    List<String> left = Arrays.asList( "b", "c");
+    List<String> right = Arrays.asList("a", "c", "d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "c", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMerge4() {
+    List<String> left = Arrays.asList( "b");
+    List<String> right = Arrays.asList("b");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("b", "b");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMerge5() {
+    List<String> left = Arrays.asList( "b", "c");
+    List<String> right = Arrays.asList();
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("b", "c");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter1() {
+    List<String> inputList = Arrays.asList("moon", "Sun", "earth", "Moon", "Mars", "moon");
+    List<String> expectedFiltered = Arrays.asList("moon", "Moon", "moon");
+    List<String> filteredList = ListExamples.filter(inputList, new IsMoon());
+    assertEquals(expectedFiltered, filteredList);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter2() {
+    List<String> inputList = Arrays.asList("Moon", "Sun", "earth", "moon", "Mars", "much");
+    List<String> expectedFiltered = Arrays.asList("Moon", "moon");
+    List<String> filteredList = ListExamples.filter(inputList, new IsMoon());
+    assertEquals(expectedFiltered, filteredList);
+  }
+}
+
